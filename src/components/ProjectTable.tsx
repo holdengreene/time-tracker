f118075589f48183c22e0b529671b8baf3d26ebc
Holden Greene <holdengreene26@gmail.com>
2025-11-08 23:56:16 -0700

    Added basic pagination

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -1,4 +1,4 @@
-import { For, Show } from "solid-js";
+import { createMemo, createSignal, For, Show } from "solid-js";
 import { activeProjectId, setActiveProjectId, setActiveProjectName } from "~/lib/global";
 import { friendlyTime } from "~/lib/util";
 import type { Project } from "~/types";
@@ -20,8 +20,61 @@ type Props = {
 
 export default function ProjectTable(props: Props) {
     const deleteProjectAction = useAction(deleteProject);
+    const projects = () => props.projects;
     let modal: HTMLDialogElement | undefined;
     let deleteProjectId: number | undefined;
+    const perPageOptions = [5, 10, 20, 50];
+
+    const [totalProjects, setTotalProjects] = createSignal<number>(projects()?.length ?? 0);
+    const [filter, setFilter] = createSignal<string>("");
+    const [projectsPerPage, setProjectsPerPage] = createSignal<number>(10);
+    const [currentPage, setCurrentPage] = createSignal<number>(1);
+
+    const totalPages = createMemo<number>(() => Math.ceil(totalProjects() / projectsPerPage()));
+
+    const pageNumbers = createMemo<number[]>(() => {
+        const pages: number[] = [];
+        const maxVisiblePages = 5;
+
+        let startPage: number;
+        let endPage: number;
+
+        if (!totalPages()) {
+            return pages;
+        }
+
+        if (totalPages() <= maxVisiblePages) {
+            startPage = 1;
+            endPage = totalProjects();
+        } else if (currentPage() <= Math.ceil(maxVisiblePages / 2)) {
+            startPage = 1;
+            endPage = maxVisiblePages;
+        } else if (
+            currentPage() + Math.floor(maxVisiblePages / 2) >=
+            totalPages()!
+        ) {
+            startPage = totalPages() - maxVisiblePages + 1;
+            endPage = totalPages();
+        } else {
+            startPage = currentPage() - Math.floor(maxVisiblePages / 2);
+            endPage = currentPage() + Math.floor(maxVisiblePages / 2);
+        }
+
+        startPage = Math.max(1, startPage);
+        endPage = Math.min(totalPages(), endPage);
+
+        for (let i = startPage; i <= endPage; i++) {
+            pages.push(i);
+        }
+
+        return pages;
+    });
+
+    const filteredProjects = createMemo<Project[] | undefined>(() => {
+        const filtered = projects()?.filter(project => project.name.toLowerCase().includes(filter()));
+
+        return filtered?.slice((currentPage() - 1) * projectsPerPage(), currentPage() * projectsPerPage());
+    });
 
     function showModal(projectId: number) {
         modal?.showModal();
@@ -33,9 +86,50 @@ export default function ProjectTable(props: Props) {
         modal?.close();
     }
 
+    const Pagination = () => <div class="pagination">
+        <div class="pagination-meta">
+            <p>Total Projects: {totalProjects()}</p>
+            <p>Page {currentPage()} of {totalPages()}</p>
+
+            <label for="per-page">Projects Per Page:</label>
+            <select id="per-page" value={projectsPerPage()} onChange={e => setProjectsPerPage(Number(e.target.value))}>
+                <For each={perPageOptions}>
+                    {option => (
+                        <option value={option}>{option}</option>
+                    )}
+                </For>
+            </select>
+
+            <label for="search">Search:</label>
+            <input id="search" type="search" value={filter()} onInput={e => setFilter(e.target.value)} />
+        </div>
+
+        <div class="pagination-controls">
+            <button classList={{ secondary: true, disabled: currentPage() === 1, pageLink: true }} onClick={() => currentPage() > 1 && setCurrentPage(currentPage() - 1)}>Prev</button>
+            <ul class="page-links-list">
+                <Show when={currentPage() - 2 > 1}>
+                    <li class="page-link"><button class="secondary" onClick={() => setCurrentPage(1)}>First</button></li>
+                </Show>
+                <For each={pageNumbers()}>
+                    {page => (
+                        <li class="page-link">
+                            <button classList={{ secondary: true, active: currentPage() === page }} onClick={() => setCurrentPage(page)}>{page}</button>
+                        </li>
+                    )}
+                </For>
+                <Show when={currentPage() + 2 < totalPages()}>
+                    <li class="page-link"><button class="secondary" onClick={() => setCurrentPage(totalPages())}>Last</button></li>
+                </Show>
+            </ul>
+            <button classList={{ secondary: true, disabled: currentPage() === totalPages(), pageLink: true }} onClick={() => currentPage() < totalPages() && setCurrentPage(currentPage() + 1)}>Next</button>
+        </div>
+    </div>;
+
     return (
         <div class="card">
             <h2>Projects</h2>
+
+            <Pagination />
             <table class="projects-table">
                 <thead>
                     <tr>
@@ -48,7 +142,7 @@ export default function ProjectTable(props: Props) {
                     </tr>
                 </thead>
                 <tbody>
-                    <For each={props.projects}>
+                    <For each={filteredProjects()}>
                         {project => (
                             <tr>
                                 <td>{project.name}</td>
